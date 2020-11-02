008f68c781faefc5738f79e659f7d9d7d2c80a9f
Jo Walsh <jowalsh@bgs.ac.uk>
2020-11-02 11:16:08 +0000

    name-similarity parts of the entity-resolver
    * This is the name matching part of the internal application
    * Only change is this version replaces the CSV dump of the Lexicon names
    / links with one generated from a SPARQL query to data.bgs.ac.uk

---
+++ tests/test_similar.py
@@ -0,0 +1,68 @@
+import pytest
+from stratigraph.similar import remove_stop_words, Similar, \
+        stopped_word, concept_index
+
+
+@pytest.fixture
+def similar():
+    return Similar()
+
+
+def test_remove_stop():
+    s = 'Patrick Burn Formation'
+    assert 'Patrick Burn' in remove_stop_words(s)
+    assert 'Form' not in remove_stop_words(s)
+
+
+def test_stopped():
+    s = 'Patrick Burn Formation'
+    assert 'Formation' in stopped_word(s)
+
+
+def test_stoppable(similar):
+    """Add 'Group' to stopwords as we catch a lot more
+    However, check afterwards that there wasn't a different word stopped"""
+    s = 'Oolite Group'
+    assert 'Oolite' in similar.most_similar(s)
+
+    s = 'Oolite Formation'
+    assert not similar.most_similar(s)
+
+
+def test_subset_similar(similar):
+    subset = [['Ree Burn',
+               'Ree Burn Formation',
+               'http://data.bgs.ac.uk/id/Lexicon/NamedRockUnit/RBN', ''],
+              ['Patrick Burn',
+               'Patrick Burn Formation',
+               'http://data.bgs.ac.uk/id/Lexicon/NamedRockUnit/PKB', '']]
+    similar.match_links = subset
+    s = 'Patrick Burn Formation'
+    print(similar.most_similar(s))
+    assert similar.most_similar(s)
+
+
+def test_most_similar(similar):
+    s = 'Patrick Burn Formation'
+    most = similar.most_similar(s)
+    assert most
+    assert remove_stop_words(s) in most
+
+
+def test_most_similar_cutoff(similar):
+    s = "Pattrick Burn Formation"
+    most = similar.most_similar(s)
+    assert 'Patrick Burn' in most
+
+    s = "Partick Burn Formation"
+    most = similar.most_similar(s)
+    assert not most
+
+    most = similar.most_similar(s, cut=0.5)
+    assert 'Patrick Burn' in most
+
+
+def test_concept_index():
+    concepts = concept_index()
+    keys = [x[0] for x in concepts]
+    assert 'Lizard Mica Schists' in keys
