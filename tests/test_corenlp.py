b07e3e4baee684df5660696f452e2c4cc4b4cb57
Jo Walsh <jowalsh@bgs.ac.uk>
2020-11-02 11:31:12 +0000

    Add the corenlp wrapper from entity-resolver

---
+++ tests/test_corenlp.py
@@ -0,0 +1,25 @@
+from stratigraph.corenlp import entities
+
+
+SAMPLE = """The data we provide includes field contect photographs of the sites and the samples that were taken, photographic logs of the drilled rock cores annotated with details of the parts that have been sampled, together with detailed geochemical analyses including XRF, XRD and ICP-MS analyses of acid dissolved samples. The 5 localities are sites where there are published records of fossil plants and paleosols. Late Silurian deposit at Bloomsburg, Pennsylvania; Lower Devonian deposit at Gaspe Bay Quebec; Miiddle Devonian forest at Cairo Quarry New York State; slightly later middle Devonian forest at Gilboa, New York; and late Devonian site at Red Hill Hyner, Pennsylvania.""" # noqa E501
+
+DEBUG_SAMPLE = "Bibliographic reference: Green, D.W. 1992. Bristol and Gloucester region. British Regional Geology. London: HMSO, 1992." # noqa E501
+
+
+def test_entities():
+    entity_doc = entities(SAMPLE)
+    assert entity_doc
+    assert 'start_char' in entity_doc[0]
+
+    entity_doc = entities('')
+    assert entity_doc == []
+
+
+def test_fullstop_bugfix():
+    entity_doc = entities('By J.R. Davies, D. Wilson, I.T. Williamson')
+    entity_doc
+    names1 = [e['name'] for e in entity_doc]
+    entity_doc = entities('By J.R. Davies, D. Wilson, I.T. Williamson.')
+    names2 = [e['name'] for e in entity_doc]
+    assert names1
+    assert names1 == names2
