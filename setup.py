63f16125655328a169d002136bcfd0ef8708ed5b
mobiuscreek <vyronc@bgs.ac.uk>
2020-11-03 10:41:14 +0000

    Add dependencies in setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'python-Levenshtein',
+        'rdflib',
+        'terminusdb-client',
+        'SPARQLWrapper'
+        ]
+
 )
 
