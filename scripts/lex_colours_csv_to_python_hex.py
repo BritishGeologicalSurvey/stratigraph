c86e4c00feccfaf42ae1c9ddb6defcb3ef908a7e
Rachel Heaven <reh@bgs.ac.uk>
2020-12-10 11:31:05 +0000

    added processing script for converting csv colours to python static - mostly lifted from another project

--- scripts/lex_colours_csv_to_python_hex.py
+++ scripts/lex_colours_csv_to_python_hex.py
@@ -1,15 +1,27 @@
+"""Processing script to convert csv file of lexicon codes and colour values to a pythonhex format.
+
+Arguments
+-i or --input_file is the input csv file
+
+Standard output
+
+redirect to a named python file  e.g. stratigraph/lex_age_colours.py from where it can be imported as a static resource in generation of stratigraphic graphs
+
+"""
+
+import argparse
 import json
 import pandas as pd
 
 BASEURL = 'http://data.bgs.ac.uk/id/Lexicon/NamedRockUnit/'
 
 
-def lex_age_colours():
-    lex = pd.read_csv('./data/lex_predominant_age_ics_colour.csv')
+def lex_colours(input_file):
+    lex = pd.read_csv(input_file)
     lex['rgb'] = list(zip(lex.RED.astype('int'),
                           lex.GREEN.astype('int'),
                           lex.BLUE.astype('int')))
-    lex['url'] = BASEURL + lex['LEX_CODE_LKD']
+    lex['url'] = BASEURL + lex['LEX']
     return lex
 
 
@@ -37,6 +49,10 @@ def colours_to_python_hex(lex):
 
 
 if __name__ == '__main__':
-    colours = lex_age_colours()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', 
+                    help="name of input csv file mapping lexicon codes to colour values; must contain fields LEX, RED, GREEN, BLUE")
+    args = parser.parse_args()
+    colours = lex_colours(args.input_file)
 #    print(colours_to_json(colours))
     print(colours_to_python_hex(colours))
