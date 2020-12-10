040b053c2cac2c6c697954dfb4c75e88fa0217ec
Rachel Heaven <reh@bgs.ac.uk>
2020-12-10 14:40:21 +0000

    using networkx isomorphism to compare graphs and fillcolor attribute

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,6 +1,7 @@
 import os
 import rdflib
 import networkx as nx
+import networkx.algorithms.isomorphism as iso
 from networkx.classes.function import is_empty
 from stratigraph.graph import triples, bounds_texts, ttl_to_nx, \
         graph_to_dot
@@ -43,10 +44,16 @@ def test_ttl_to_nx():
     assert not is_empty(nx_graph)
 
     # Case in which we use the age colour scale
-    nx_graph = ttl_to_nx(triples=triples, colour_scale='age')
-    assert not is_empty(nx_graph)
+    nx_graph_age = ttl_to_nx(triples=triples, colour_scale='age')
+    assert not is_empty(nx_graph_age)
+    
+    # Check graphs created with different colour scale are the same if not comparing attributes
     nx_graph_digmap = ttl_to_nx(triples=triples)
-    assert nx_graph != nx_graph_digmap
+    assert nx.is_isomorphic(nx_graph_digmap,nx_graph_age)
+    
+    # Check graphs created with different colour scale are are different when also comparing attributes
+    nm = iso.categorical_node_match('fillcolor', '#EEEEEE')
+    assert not nx.is_isomorphic(nx_graph_digmap,nx_graph_age, node_match=nm)
 
 def test_graph_to_dot():
     triples = os.path.join(os.path.dirname(__file__),
