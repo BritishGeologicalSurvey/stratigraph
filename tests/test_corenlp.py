a3cdf824cc56cf05ef1038b4044c22e63b84f4e5
mobiuscreek <vyronc@bgs.ac.uk>
2021-01-12 12:26:48 +0000

    Add more corenlp tests

--- tests/test_corenlp.py
+++ tests/test_corenlp.py
@@ -1,10 +1,23 @@
-from stratigraph.corenlp import entities
+import pytest
+from unittest.mock import patch
+from requests.exceptions import ConnectionError
 
+from stratigraph.corenlp import entities, tokens
 
 SAMPLE = """The data we provide includes field contect photographs of the sites and the samples that were taken, photographic logs of the drilled rock cores annotated with details of the parts that have been sampled, together with detailed geochemical analyses including XRF, XRD and ICP-MS analyses of acid dissolved samples. The 5 localities are sites where there are published records of fossil plants and paleosols. Late Silurian deposit at Bloomsburg, Pennsylvania; Lower Devonian deposit at Gaspe Bay Quebec; Miiddle Devonian forest at Cairo Quarry New York State; slightly later middle Devonian forest at Gilboa, New York; and late Devonian site at Red Hill Hyner, Pennsylvania.""" # noqa E501
 
 DEBUG_SAMPLE = "Bibliographic reference: Green, D.W. 1992. Bristol and Gloucester region. British Regional Geology. London: HMSO, 1992." # noqa E501
 
+
+@patch("stratigraph.corenlp.SERVER", "http://localhost:1234")
+def test_tokens_exception():
+    with pytest.raises(ConnectionError):
+         tokens(SAMPLE)
+
+def test_tokens_works():
+    result = tokens(SAMPLE)
+    assert isinstance(result, dict)
+    assert 'sentences' in result
 
 def test_entities():
     entity_doc = entities(SAMPLE)
